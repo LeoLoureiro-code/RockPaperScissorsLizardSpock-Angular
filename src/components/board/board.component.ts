efa48ef639d489f340dcdd8bbda4071a5aebb39b
Leo Loureiro <leolou.90@gmail.com>
2025-05-20 19:03:57 -0700

    add options to imports

--- src/components/board/board.component.ts
+++ src/components/board/board.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ScoreComponent } from "../score/score.component";
+import { OptionsComponent } from "../options/options.component";
 
 @Component({
   selector: 'board',
   standalone: true,
-  imports: [ScoreComponent],
+  imports: [ScoreComponent, OptionsComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
