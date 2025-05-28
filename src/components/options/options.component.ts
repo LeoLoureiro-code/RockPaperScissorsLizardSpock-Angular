84820d5d28e112aa1bd6458ce03dd7f74820f712
Leo Loureiro <leolou.90@gmail.com>
2025-05-27 19:04:57 -0700

    add fuction to emit the option

--- src/components/options/options.component.ts
+++ src/components/options/options.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter} from '@angular/core'; 
 import { OptionComponent } from '../option/option.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'options',
   standalone: true,
-  imports: [OptionComponent],
+  imports: [OptionComponent, CommonModule],
   templateUrl: './options.component.html',
   styleUrl: './options.component.scss'
 })
 export class OptionsComponent {
 
+  @Output() optionSelected = new EventEmitter<string>();
+
+  onSelect(option: string) {
+    this.optionSelected.emit(option);
+  }
 }
