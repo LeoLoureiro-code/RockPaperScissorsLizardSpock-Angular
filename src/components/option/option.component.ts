18dc3598caaf37dae0e70145c69ea5205c3ccac7
Leo Loureiro <leolou.90@gmail.com>
2025-05-27 19:04:16 -0700

    add function to choose an option

--- src/components/option/option.component.ts
+++ src/components/option/option.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,9 +9,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './option.component.scss'
 })
 export class OptionComponent { 
-
+  @Output() selected = new EventEmitter<string>();
   @Input() name!: string;
   @Input() color?: string;
   @Input() imageUrl?: string;
+
+  chooseOption() {
+  this.selected.emit(this.name);
+}
 
 }
