ca9af382cd184d1eeed6c2632be3b0b7e48ef8f1
Leo Loureiro <leolou.90@gmail.com>
2025-05-27 19:03:16 -0700

    add function to open and close modal

--- src/components/board/board.component.ts
+++ src/components/board/board.component.ts
@@ -1,14 +1,35 @@
 import { Component } from '@angular/core';
 import { ScoreComponent } from "../score/score.component";
 import { OptionsComponent } from "../options/options.component";
+import { ModalComponent } from '../modal/modal.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'board',
   standalone: true,
-  imports: [ScoreComponent, OptionsComponent],
+  imports: [ScoreComponent, OptionsComponent, ModalComponent, CommonModule],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
 export class BoardComponent {
 
+  gameState: 'select' | 'animating' | 'result' = 'select';
+  playerChoice: string = '';
+
+  isModalOpen = false;
+  modalImageUrl = '';
+
+  handleOptionSelection(option: string) {
+  console.log('User selected:', option);
+  }
+
+  openModal(imageUrl: string) {
+    this.modalImageUrl = imageUrl;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
