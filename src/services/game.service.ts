55148c73632501dd6f1531472e3e7c6bd832a7f3
Leo Loureiro <leolou.90@gmail.com>
2025-06-01 19:25:24 -0700

    add service to determine winner and change score (just need to show it)

--- src/services/game.service.ts
+++ src/services/game.service.ts
@@ -1,17 +1,46 @@
 import { Injectable } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GameService implements OnInit {
+export class GameService {
 
-  playerChoise! :string;
-  CPUChoise! :string;
-  
-  
-  constructor() { }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+  private scoreSubject = new BehaviorSubject<number>(0);
+
+  options = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+
+  getRandomOption(): string {
+    const index = Math.floor(Math.random() * this.options.length);
+    return this.options[index];
   }
+
+  determineWinner(user: string, cpu: string): 'user' | 'cpu' | 'tie' {
+    const winsAgainst: Record<string, string[]> = {
+      rock: ['scissors', 'lizard'],
+      paper: ['rock', 'spock'],
+      scissors: ['paper', 'lizard'],
+      lizard: ['spock', 'paper'],
+      spock: ['scissors', 'rock'],
+    };
+
+    if (user === cpu) return 'tie';
+    if (winsAgainst[user].includes(cpu)) return 'user';
+    return 'cpu';
+  }
+
+  GetScore(): Observable<number> {
+  return this.scoreSubject.asObservable();
+}
+
+  IncrementScore() {
+  this.scoreSubject.next(this.scoreSubject.value + 1);
+}
+
+  DecrementScore(){
+    if(this.scoreSubject.value > 0){
+      this.scoreSubject.next(this.scoreSubject.value - 1);
+    }
+  }
+  
 }
