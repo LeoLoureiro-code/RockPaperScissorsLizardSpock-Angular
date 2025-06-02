050007d73abb2b52c308c8e6134311368ee50664
Leo Loureiro <leolou.90@gmail.com>
2025-06-01 19:23:24 -0700

    add styles to buttons and conditional rendering to change board after select option

--- src/components/board/board.component.ts
+++ src/components/board/board.component.ts
@@ -1,28 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ScoreComponent } from "../score/score.component";
 import { OptionsComponent } from "../options/options.component";
 import { ModalComponent } from '../modal/modal.component';
 import { CommonModule } from '@angular/common';
+import { GameService } from '../../services/game.service';
+import { OptionComponent } from '../option/option.component';
 
 
 @Component({
   selector: 'board',
   standalone: true,
-  imports: [ScoreComponent, OptionsComponent, ModalComponent, CommonModule],
+  imports: [ScoreComponent, OptionsComponent, ModalComponent, CommonModule, OptionComponent],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
-export class BoardComponent {
+export class BoardComponent implements OnInit{
 
-  gameState: 'select' | 'animating' | 'result' = 'select';
-  playerChoice: string = '';
+score = 0;
 
-  isModalOpen = false;
-  modalImageUrl = '';
+selectedOption: string | null = null;
+cpuOption: string | null = null;
+winner: 'user' | 'cpu' | 'tie' | null = null;
 
-  handleOptionSelection(option: string) {
-  console.log('User selected:', option);
+isModalOpen = false;
+modalImageUrl = '';
+
+constructor(private gameService: GameService) {}
+
+ngOnInit() {
+  this.gameService.GetScore().subscribe(scoreValue => {
+    this.score = scoreValue;
+  });
+}
+
+handleOptionSelection(userOption: string) {
+  this.selectedOption = userOption;
+  this.cpuOption = this.gameService.getRandomOption();
+  this.winner = this.gameService.determineWinner(userOption, this.cpuOption);
+
+  if (this.winner === 'user') {
+    this.gameService.IncrementScore();
+  } else if (this.winner === 'cpu') {
+    this.gameService.DecrementScore();
   }
+}
+
+
+
+resetGame() {
+  this.selectedOption = null;
+  this.cpuOption = null;
+  this.winner = null;
+}
 
   openModal(imageUrl: string) {
     this.modalImageUrl = imageUrl;
--- src/components/options/options.component.ts
+++ src/components/options/options.component.ts
@@ -16,4 +16,6 @@ export class OptionsComponent {
   onSelect(option: string) {
     this.optionSelected.emit(option);
   }
+
+
 }
