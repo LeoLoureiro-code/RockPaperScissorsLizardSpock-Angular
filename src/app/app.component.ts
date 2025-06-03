7e27c51f4bbab3a221d313dd66341853ef7e48c9
Leo Loureiro <leolou.90@gmail.com>
2025-06-02 19:11:08 -0700

    add imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BoardComponent } from "../components/board/board.component";
+import { ScoreComponent } from "../components/score/score.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BoardComponent, ScoreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'RockPaperScissorsLizardSpock';
+
 }
